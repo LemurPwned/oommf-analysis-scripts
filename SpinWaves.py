9c666f92e3930b56872ea223f6355ce1e2010430
LemurPwned <jakub.mojsiejuk@gmail.com>
2019-01-06 13:15:51 +0100

    adding animation saving for spin waves

--- SpinWaves.py
+++ SpinWaves.py
@@ -42,9 +42,9 @@ class SpinWave(AnalysisUnit):
         if self.write:
             Writer = animation.writers['ffmpeg']
             self.writer = Writer(
-                fps=15, metadata=dict(artist='Me'), bitrate=1800)
+                fps=15, metadata=dict(artist='LemurPwned'), bitrate=1800)
 
-   def plot_waves(self):
+    def plot_waves(self):
         self.file_names = list(map(lambda x: os.path.join(self.directory, x),
                                    sorted(filter(lambda x: x.endswith('.ovf'),
                                                  os.listdir(self.directory)))))
@@ -58,18 +58,19 @@ class SpinWave(AnalysisUnit):
                                    int(header['xnodes']), 3))
 
         fig = plt.figure()
-        self.ax = plt.subplot2grid((5, 5), (0, 0), colspan=5,
-                                   rowspan=3)  # axes_plot
-        ax_bl = plt.subplot2grid(
-            (5, 5), (4, 0), colspan=2, rowspan=1)  # axes_button_left
-        ax_br = plt.subplot2grid((5, 5), (4, 3), colspan=2, rowspan=1)
+        if self.write:
+            self.ax = fig.add_subplot(111)
+        else:
+            self.ax = plt.subplot2grid((5, 5), (0, 0), colspan=5,
+                                       rowspan=3)  # axes_plot
+            ax_bl = plt.subplot2grid(
+                (5, 5), (4, 0), colspan=2, rowspan=1)  # axes_button_left
+            ax_br = plt.subplot2grid((5, 5), (4, 3), colspan=2, rowspan=1)
+            butt_l = Button(ax_bl, '\N{leftwards arrow}')  # or u'' on python 2
+            butt_r = Button(ax_br, '\N{rightwards arrow}')
 
-        butt_l = Button(ax_bl, '\N{leftwards arrow}')  # or u'' on python 2
-        butt_r = Button(ax_br, '\N{rightwards arrow}')
-
-        butt_l.on_clicked(self.left_onclicked)
-        butt_r.on_clicked(self.right_onclicked)
-
+            butt_l.on_clicked(self.left_onclicked)
+            butt_r.on_clicked(self.right_onclicked)
         m = vectors[0, 0, :, self.m_component]
         x_vals = [i*header['xstepsize'] for i in
                   range(int(header['xnodes']))]
@@ -79,9 +80,16 @@ class SpinWave(AnalysisUnit):
         self.ax.set_ylabel(self.component)
         self.ax.set_title(f"{self.i}/{self.total_its}")
         self.ax.axis([0, np.max(x_vals), -1.1, 1.1])
+
+        if self.write:
+            anim = animation.FuncAnimation(fig, self.replot_data, int(self.total_its/self.step),
+                                           interval=50, blit=False)
+            anim.save('spins.mp4', writer=self.writer)
+            return
+
         plt.show()
 
-    def replot_data(self):
+    def replot_data(self, num=-1):
         '''replot data after button push, assumes constant data shape'''
         header, vectors = ParsingUtils.binary_format_reader(
             filename=self.file_names[self.i])
@@ -93,6 +101,10 @@ class SpinWave(AnalysisUnit):
         self.ax.hpl.set_ydata(m)
         self.ax.set_title(f"{self.i}/{self.total_its}")
         self.ax.get_figure().canvas.draw()
+        if num != -1:
+            if self.i + self.step < self.total_its:
+                self.i += self.step
+            return self.ax,
 
     def left_onclicked(self, event):
         '''try to decrement data index, replot if success'''
